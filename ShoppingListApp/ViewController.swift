9f500a9d1d714610fa005918c220edfadbc44eb5
Vishal <vishalpatel151196@gmail.com>
2019-10-16 20:43:16 -0400

    FirstCommit

--- ShoppingListApp/ViewController.swift
+++ ShoppingListApp/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var qty1: UITextField!
+    
+    @IBOutlet weak var stepper1: UIStepper!
+    
+    
+    @IBAction func stepper1Action(_ sender: Any) {
+        qty1.text = String(stepper1.value)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
